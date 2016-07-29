dad714d825c9ae2df82c347c9e180b5302a7beef
halohoop <halohoop@163.com>
2016-07-29 19:20:35 +0800

    Initial commit

---
+++ middlelinesdialogutils/src/main/java/com/halohoop/middlelinesdialogutils/dialogs/ItemClickListener.java
@@ -0,0 +1,8 @@
+package com.halohoop.middlelinesdialogutils.dialogs;
+
+/**
+ * Created by Pooholah on 2016/7/15.
+ */
+public interface ItemClickListener {
+    void onClick(int position,int resIdDialogBelongTo);
+}
